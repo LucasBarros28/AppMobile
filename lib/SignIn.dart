f6fe279bf5c92544a1438d0fdae3b46e2ae5d676
Alex Marques <alex@recriarsistemas.com.br>
2025-04-02 20:13:02 -0300

    fixed sign in

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -26,157 +26,155 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFF57362B),
-        body: Center(
-          child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                              child: Column(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(
-                                    20.0), // Define o raio das bordas
-                                child: Image.asset(
-                                  'assets/images/logoTeste.png',
-                                  width: 200.0,
-                                  height: 200.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                "Lecternus",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )
-                            ],
-                          )),
-                          SizedBox(height: 10,),
-                          Text(
-                            "E-mail/Nome de usuario",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          TextField(
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0xFFCDA68C),
-                              border: OutlineInputBorder(),
-                              hintText: "Digite seu e-mail ou nome de usuario",
-                              hintStyle: TextStyle(
-                          color: const Color(0xFF57362B),
-                        ),
-                              prefixIcon: Icon(Icons.email, color: const Color(0xFF57362B)),
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Senha",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          TextField(
-                            obscureText: _obscureText,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0xFFCDA68C),
-                              border: OutlineInputBorder(),
-                              hintText: "Digite sua senha",
-                        hintStyle: TextStyle(
-                          color: const Color(0xFF57362B),
-                        ),
-                              prefixIcon: Icon(Icons.lock, color: const Color(0xFF57362B),),
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _obscureText
-                                      ? Icons.visibility_off
-                                      : Icons.visibility,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _obscureText = !_obscureText;
-                                  });
-                                },
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 15,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            MainScreen(initialIndex: 0)),
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xFFCDA68C),
-                                  foregroundColor: Colors.white,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 40, vertical: 12),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                child: Text(
-                                  "Entrar",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => SignUp()),
-                                  );
-                                },
-                                child: Text(
-                                  "Ainda não tem uma conta? Cadastre-se",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      decoration: TextDecoration.underline,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+      backgroundColor: const Color(0xFF57362B),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 125, left: 16, right: 16, bottom: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                  child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(20.0), // Define o raio das bordas
+                    child: Image.asset(
+                      'assets/images/logoTeste.png',
+                      width: 200.0,
+                      height: 200.0,
+                      fit: BoxFit.cover,
                     ),
-                  ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Lecternus",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "E-mail/Nome de usuario",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
                 ),
               ),
+              SizedBox(height: 5),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFCDA68C),
+                  border: OutlineInputBorder(),
+                  hintText: "Digite seu e-mail ou nome de usuario",
+                  hintStyle: TextStyle(
+                    color: const Color(0xFF57362B),
+                  ),
+                  prefixIcon: Icon(Icons.email, color: const Color(0xFF57362B)),
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "Senha",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 5),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFCDA68C),
+                  border: OutlineInputBorder(),
+                  hintText: "Digite sua senha",
+                  hintStyle: TextStyle(
+                    color: const Color(0xFF57362B),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: const Color(0xFF57362B),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MainScreen(initialIndex: 0)),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFCDA68C),
+                      foregroundColor: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    child: Text(
+                      "Ainda não tem uma conta? Cadastre-se",
+                      style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        );
+      ),
+    );
   }
 }
