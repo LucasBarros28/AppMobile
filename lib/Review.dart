383d025d467569257574c9637c53831618c7ee8f
LucasBarros28 <lukasf28.28lfb@gmail.com>
2025-04-01 22:20:31 -0300

    Tela de review

--- lib/Review.dart
+++ lib/Review.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'Config.dart';
-import 'Sobre.dart';
 import 'bottom_nav_bar.dart';
-import 'Home.dart';
 import 'main.dart';
 
 class Review extends StatefulWidget {
+  final String reviewText;
+
+  Review({required this.reviewText});
+
   @override
   _ReviewState createState() => _ReviewState();
 }
 
 class _ReviewState extends State<Review> {
   int _selectedIndex = 2;
+  TextEditingController _commentController = TextEditingController();
 
   void _onItemTapped(int index) {
     if (index != _selectedIndex) {
@@ -28,7 +31,7 @@ class _ReviewState extends State<Review> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF6D3701),
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
         title: Text(
           "Lecternus",
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -54,105 +57,100 @@ class _ReviewState extends State<Review> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Stack(
-              clipBehavior: Clip.none,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Alinha os itens no topo
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 5,
-                            spreadRadius: 2,
-                          ),
-                        ],
+                Container(
+                  width: 150,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 5,
+                        spreadRadius: 2,
                       ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          'assets/images/imagem.jpg', // Caminho da sua imagem
-                          fit: BoxFit.cover,
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/images/imagem.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nome do Livro: Mar azul',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                        width: 16), // Espaço entre a foto e os campos de texto
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment
-                            .start, // Alinha os itens à esquerda
-                        children: [
-                          Text(
-                            'Nome do Livro:',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black54,
-                            ),
-                          ),
-                          SizedBox(height: 20), // Espaço entre os campos
-                          Text(
-                            'Nome do Autor:',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ],
+                      SizedBox(height: 20),
+                      Text(
+                        'Nome do Autor: José da Silva',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
             SizedBox(height: 20),
-            Text(
-              'Texto Grande:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black54,
-              ),
-            ),
-            TextField(
-              maxLines: 5,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFFAC842D),
-                hintText: 'Digite um texto grande aqui...',
-                border: OutlineInputBorder(),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                widget.reviewText,
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                textAlign: TextAlign.justify,
               ),
             ),
             SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity, // Largura do botão ocupa toda a tela
-              child: ElevatedButton(
-                onPressed: () {
-                  // Ação para salvar
-                  print("Salvar pressionado");
-                },
-                child: Text(
-                  'Salvar',
-                  style: TextStyle(
-                      color: Colors.white), // Cor do texto para branco
+            Container(
+              width:
+                  double.infinity, // Faz o campo de texto ocupar toda a largura
+              height: 50, // Ajuste a altura para algo semelhante ao botão
+              padding: EdgeInsets.symmetric(
+                  horizontal: 12), // Mantém o padding lateral
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(color: Colors.black38),
+              ),
+              child: TextField(
+                controller: _commentController,
+                maxLines: 1,
+                decoration: InputDecoration(
+                  hintText: 'Escreva seu comentário aqui...',
+                  border: InputBorder.none,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.arrow_forward, // Ícone de seta
+                      color: Colors.black87, // Cor da seta
+                    ),
+                    onPressed: () {
+                      // Adicione aqui a lógica para o que acontecerá quando o botão for pressionado
+                      print("Comentário enviado: ${_commentController.text}");
+                    },
+                  ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFAC842D), // Cor do botão
-                  padding:
-                      EdgeInsets.symmetric(vertical: 15), // Espaçamento interno
-                  textStyle: TextStyle(fontSize: 16), // Estilo do texto
-                ),
+                style: TextStyle(fontSize: 16, color: Colors.black87),
               ),
             ),
+            SizedBox(height: 20),
           ],
         ),
       ),
